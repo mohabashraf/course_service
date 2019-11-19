d6aa769f075b0b20810e9f01ee3e44fbc464d1e1
mohabashraf <mohabashraf466@gmail.com>
2019-11-19 15:35:45 +0200

    Create instructor model

--- src/main/java/course/course_service/domain/Instructor.java
+++ src/main/java/course/course_service/domain/Instructor.java
@@ -1,6 +1,27 @@
 package course.course_service.domain;
 
+import org.springframework.data.annotation.Id;
+
 public class Instructor {
 
-    private String ID
+    @Id
+    private String ID;
+
+    private String name;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
