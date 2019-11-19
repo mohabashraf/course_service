f4c0872e54847a87168fd6a587db46609d70f248
mohabashraf <mohabashraf466@gmail.com>
2019-11-19 15:44:20 +0200

    create Course  model

--- src/main/java/course/course_service/domain/Course.java
+++ src/main/java/course/course_service/domain/Course.java
@@ -1,4 +1,40 @@
 package course.course_service.domain;
 
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
 public class Course {
+
+    @Id
+    private String ID;
+
+    private Instructor instructor;
+
+    private List<Student> enrolledStudents;
+
+    public String getID() {
+        return ID;
+    }
+
+    public List<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void addStudent(Student student) {
+        this.enrolledStudents.add(student);
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
 }
