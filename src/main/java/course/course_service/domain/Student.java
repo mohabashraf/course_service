c1105defddaa2629caf3716f597a27f7284a5d0f
mohabashraf <mohabashraf466@gmail.com>
2019-11-19 15:39:14 +0200

    create Student model

--- src/main/java/course/course_service/domain/Student.java
+++ src/main/java/course/course_service/domain/Student.java
@@ -1,4 +1,27 @@
 package course.course_service.domain;
 
+import org.springframework.data.annotation.Id;
+
 public class Student {
+
+    @Id
+    private String ID;
+
+    private String name;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
